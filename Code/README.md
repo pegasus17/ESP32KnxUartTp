bf0ff65d95a011822d9a53661d32225f5afebb5c
Roland Gärtner <roland@gardeners.at>
2022-03-20 08:39:25 +0100

    Connection to Wifi

---
+++ Code/README.md
@@ -0,0 +1,6 @@
+# Firmware for KNX Buddy
+
+## Preperation
+
+As first step you need to copy the file ```include/wifi_config.template``` to ```include/wifi_config.h```
+and change the settings to our Wifi network.
--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,9 +1,62 @@
 #include <Arduino.h>
+#include "wifi_config.h"
+#include <WiFi.h>
+
+static void connectToWiFi();
+static void WiFiEvent(WiFiEvent_t event);
 
 void setup() {
-  // put your setup code here, to run once:
-}
+  Serial.begin(115200);
+  Serial.println("Starting ..");
+
+  connectToWiFi();
+} 
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+/**
+ * @brief Establish connection to the network
+ * 
+ */
+static void connectToWiFi() {
+  // close existing connection if there is one
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect(true, true);
+  delay(100);
+  // register event handler
+  WiFi.onEvent(WiFiEvent);
+
+  Serial.println("ESP Board MAC Address:  " + WiFi.macAddress());
+
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+}
+
+/**
+ * @brief wifi event handler
+ * 
+ * @param event  [in]
+ */
+static void WiFiEvent(WiFiEvent_t event)
+{
+  switch (event)
+  {
+  case SYSTEM_EVENT_STA_GOT_IP:
+    Serial.print("WiFi connected: RRSI=");
+    Serial.print(WiFi.RSSI());
+    Serial.print(", IP=");
+    Serial.println(WiFi.localIP());
+    break;
+
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    Serial.println("WiFi lost connection");
+    break;
+
+  default:
+    break;
+  }
 }
